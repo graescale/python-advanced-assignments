3d062db2ad107ad7ec675c2ea023df43f48d96bb
Grae Revell <grae.revell@gmail.com>
2021-12-21 17:13:47 -0500

    OPTIMIZE compress_ifs.py

--- 02_style/compress_ifs.py
+++ 02_style/compress_ifs.py
@@ -11,33 +11,13 @@
 
 # COMMENT --------------------------------------------------
 # Not optimal
-def set_color(ctrlList=None, color=None):
+def set_color(ctrlList, color):
+
+    color_overrides = {1:4, 2:13, 3:25, 4:17, 5:17, 6:15, 7:6, 8:16}
 
     for ctrlName in ctrlList:
-        try:
-            mc.setAttr(ctrlName + 'Shape.overrideEnabled', 1)
-        except:
-            pass
+       mc.setAttr(ctrlName + 'Shape.overrideColor', color_overrides[color])
 
-        try:
-            if color == 1:
-                mc.setAttr(ctrlName + 'Shape.overrideColor', 4)
-            elif color == 2:
-                mc.setAttr(ctrlName + 'Shape.overrideColor', 13)
-            elif color == 3:
-                mc.setAttr(ctrlName + 'Shape.overrideColor', 25)
-            elif color == 4:
-                mc.setAttr(ctrlName + 'Shape.overrideColor', 17)
-            elif color == 5:
-                mc.setAttr(ctrlName + 'Shape.overrideColor', 17)
-            elif color == 6:
-                mc.setAttr(ctrlName + 'Shape.overrideColor', 15)
-            elif color == 7:
-                mc.setAttr(ctrlName + 'Shape.overrideColor', 6)
-            elif color == 8:
-                mc.setAttr(ctrlName + 'Shape.overrideColor', 16)
-        except:
-            pass
 
 
 # EXAMPLE
